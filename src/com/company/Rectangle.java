eefe4a7222ca04e83f84646c8e72b1f745b11007
muratbaiguchukov <ambkg-muratb@yandex.ru>
2022-01-23 21:16:40 +0600

    add rectangle

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Rectangle {
+    public int heigth;
+    public int width;
+
+    public Rectangle(int w, int h) {
+        width = w;
+        heigth = h;
+    }
+
+    public int getHeigth;
+}
