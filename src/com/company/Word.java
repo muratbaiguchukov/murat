7b69c48cf94f79c796f1b7149397a2713b96ac5c
muratbaiguchukov <ambkg-muratb@yandex.ru>
2022-02-22 19:28:02 +0600

    add exam-1-zad-3

---
+++ src/com/company/Word.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.*;
+
+public class Word {
+    public static void main(String[] args) {
+
+        char[] chars = {'п', 'р', 'о', 'г', 'р', 'а', 'м', 'м', 'и', 'с', 'т'};
+        for (int i = chars.length-1; i >= 0; i--)
+        System.out.print(chars[i]);
+
+        }
+    }
+
